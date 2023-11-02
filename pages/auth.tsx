16e17b2cb6dac7b079ba4df8de3bcfb0ad917b10
Suman <sm7135120@gmail.com>
2023-11-02 13:22:31 +0530

    added profile

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
+import axios from "axios";
 import Input from "@/components/input";
 import { useCallback, useState } from "react";
+import { signIn } from "next-auth/react";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
 	const [email, setEmail] = useState("");
@@ -14,6 +19,31 @@ const Auth = () => {
 			currentVarient === "login" ? "register" : "login"
 		);
 	}, []);
+
+	const logIn = useCallback(async () => {
+		try {
+			await signIn("credentials", {
+				email,
+				password,
+				callbackUrl: "/profiles",
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}, [email, password]);
+
+	const register = useCallback(async () => {
+		try {
+			await axios.post("/api/register", {
+				email,
+				name,
+				password,
+			});
+			logIn();
+		} catch (error) {
+			console.log(error);
+		}
+	}, [email, name, password, logIn]);
 
 	return (
 		<div className="realtive h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -54,16 +84,38 @@ const Auth = () => {
 								value={password}
 							/>
 						</div>
-						<button className=" bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+						<button
+							onClick={varient === "login" ? logIn : register}
+							className=" bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
 							{varient === "login" ? "Login" : "Sign up"}
 						</button>
+						<div className=" flex flex-row items-center gap-4 mt-8 justify-center">
+							<div
+								onClick={() =>
+									signIn("github", {
+										callbackUrl: "/profiles",
+									})
+								}
+								className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+								<FaGithub size={30} />
+							</div>
+							<div
+								onClick={() =>
+									signIn("google", {
+										callbackUrl: "/profiles",
+									})
+								}
+								className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+								<FcGoogle size={30} />
+							</div>
+						</div>
 						<p className=" text-neutral-500 mt-12">
 							{varient === "login"
 								? "First time using Netflix?"
 								: "Already have an account?"}
 							<span
 								onClick={toggleVarient}
-								className="text-white ml-1 hover:underline cursor-pointer ">
+								className="text-white ml-1 hover:underline cursor-pointer">
 								{varient === "login" ? "Create an account" : "Login"}
 							</span>
 						</p>
